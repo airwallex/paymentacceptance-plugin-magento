1eddc1d26d675b09ed9e2d32fe84b124a2a12b6e
Edgars Patmalnieks <edgars.patmalnieks@magebit.com>
2023-05-20 10:58:33 +0300

    MB-22781 fixes infinite loader on some errors

--- view/frontend/web/js/view/payment/method-renderer/card-method.js
+++ view/frontend/web/js/view/payment/method-renderer/card-method.js
@@ -69,6 +69,7 @@ define([
                     }.bind(this))
                     .catch(function (response) {
                         this.validationError(response.message);
+                        $('body').trigger('processStop');
                     }.bind(this));
             },
 
