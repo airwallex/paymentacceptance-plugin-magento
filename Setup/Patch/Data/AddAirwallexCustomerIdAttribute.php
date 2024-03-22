cb8de886d32906d969c10e8c22283a8398325f2d
Andris Bremanis <andris.bremanis@magebit.com>
2024-03-22 17:31:14 +0200

    MB-25550 - Add airwallex_customer_id attribute

---
+++ Setup/Patch/Data/AddAirwallexCustomerIdAttribute.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * This file is part of the Airwallex Payments module.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade
+ * to newer versions in the future.
+ *
+ * @copyright Copyright (c) 2021 Magebit, Ltd. (https://magebit.com/)
+ * @license   GNU General Public License ("GPL") v3.0
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Airwallex\Payments\Setup\Patch\Data;
+
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Validator\ValidateException;
+
+class AddAirwallexCustomerIdAttribute implements DataPatchInterface
+{
+    private ModuleDataSetupInterface $moduleDataSetup;
+    private EavSetupFactory $eavSetupFactory;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * @throws LocalizedException
+     * @throws ValidateException
+     */
+    public function apply()
+    {
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute(Customer::ENTITY, 'airwallex_customer_id', [
+            'type' => 'varchar',
+            'label' => 'Airwallex Customer ID',
+            'input' => 'text',
+            'required' => false,
+            'visible' => false,
+            'user_defined' => false,
+            'position' => 999,
+            'system' => 0,
+            'is_used_in_grid' => true,
+            'is_visible_in_grid' => true,
+            'is_filterable_in_grid' => true,
+            'is_searchable_in_grid' => true,
+        ]);
+    }
+
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+}
