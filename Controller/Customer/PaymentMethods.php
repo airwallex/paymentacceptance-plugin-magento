d6d9dfc6f66511980512bc5704e368531c59745c
Aija Pastare <aija.pastare@magebit.com>
2024-04-15 12:31:56 +0300

    MB-25572 Add docblocks

--- Controller/Customer/PaymentMethods.php
+++ Controller/Customer/PaymentMethods.php
@@ -1,6 +1,4 @@
 <?php
-declare(strict_types=1);
-
 /**
  * This file is part of the Airwallex Payments module.
  *
@@ -9,12 +7,14 @@ declare(strict_types=1);
  * Do not edit or add to this file if you wish to upgrade
  * to newer versions in the future.
  *
- * @copyright Copyright (c) 2024 Magebit, Ltd. (https://magebit.com/)
+ * @copyright Copyright (c) 2021 Magebit, Ltd. (https://magebit.com/)
  * @license   GNU General Public License ("GPL") v3.0
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+declare(strict_types=1);
 
 namespace Airwallex\Payments\Controller\Customer;
 
