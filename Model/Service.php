157ea9738d0e52e2958133a54f3e0af56a2abcb2
leon-zhang-awx <leon.zhang.ctr@airwallex.com>
2024-06-20 13:01:27 +0800

    update order

--- Model/Service.php
+++ Model/Service.php
@@ -665,8 +665,9 @@ class Service implements ServiceInterface
         $resp = $this->intentGet->setPaymentIntentId($id)->send();
 
         $respArr = json_decode($resp, true);
+        $quote = $this->checkoutHelper->getQuote();
         $okStatus = [$this->intentGet::INTENT_STATUS_SUCCESS, $this->intentGet::INTENT_STATUS_REQUIRES_CAPTURE];
-        if (!in_array($respArr['status'], $okStatus, true)) {
+        if (!in_array($respArr['status'], $okStatus, true) || $respArr['merchant_order_id'] !== $quote->getReservedOrderId()) {
             $msg = 'Something went wrong while processing your request. Please try again later.';
             throw new Exception(__($msg));
         }
