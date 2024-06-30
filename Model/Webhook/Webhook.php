26aac6cc0bf19a1059b7e44fbb0fbd89e919453b
leon-zhang-awx <leon.zhang.ctr@airwallex.com>
2024-06-30 21:20:25 +0800

    add AVS when order placed

--- Model/Webhook/Webhook.php
+++ Model/Webhook/Webhook.php
@@ -25,11 +25,13 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Airwallex\Payments\Model\PaymentIntentRepository;
 use stdClass;
 use Airwallex\Payments\Model\Client\Request\PaymentIntents\Get;
-use Magento\Sales\Model\Order;
+use Airwallex\Payments\Model\Traits\HelperTrait;
 use Magento\Sales\Model\OrderRepository;
 
 class Webhook
 {
+    use HelperTrait;
+
     private const HASH_ALGORITHM = 'sha256';
 
     public const AUTHORIZED_WEBHOOK_NAMES = ['payment_intent.requires_capture'];
@@ -142,48 +144,10 @@ class Webhook
 
         if (in_array($type, Capture::WEBHOOK_NAMES)) {
             $this->capture->execute($data);
-            $this->addAVSResult($data);
-        }
-
-        if (in_array($type, self::AUTHORIZED_WEBHOOK_NAMES)) {
-            $this->addAVSResult($data);
         }
 
         if ($type === Cancel::WEBHOOK_NAME) {
             $this->cancel->execute($data);
         }
-    }
-
-    protected function addAVSResult($data)
-    {
-        $id = $data->payment_intent_id ?? $data->id;
-        /** @var Order $order */
-        $order = $this->paymentIntentRepository->getOrder($id);
-        if (!$order) return;
-        $histories = $order->getStatusHistories();
-        if (!$histories) {
-            return;
-        }
-        $log = $src = '[Verification] ';
-        foreach ($histories as $history) {
-            $history->getComment();
-            if (strstr($history->getComment(), $log)) return;
-        }
-        try {
-            $resp = $this->intentGet->setPaymentIntentId($id)->send();
-            $respArr = json_decode($resp, true);
-            $brand = $respArr['latest_payment_attempt']['payment_method']['card']['brand'] ?? '';
-            if ($brand) $brand = ' Card Brand: ' . strtoupper($brand) . '.';
-            $last4 = $respArr['latest_payment_attempt']['payment_method']['card']['last4'] ?? '';
-            if ($last4) $last4 = ' Card Last Digits: ' . $last4 . '.';
-            $avs_check = $respArr['latest_payment_attempt']['authentication_data']['avs_result'] ?? '';
-            if ($avs_check) $avs_check = ' AVS Result: ' . $avs_check . '.';
-            $cvc_check = $respArr['latest_payment_attempt']['authentication_data']['cvc_result'] ?? '';
-            if ($cvc_check) $cvc_check = ' CVC Result: ' . $cvc_check . '.';
-            $log .= $brand . $last4 . $avs_check . $cvc_check;
-            if ($log === $src) return;
-            $order->addCommentToStatusHistory(__($log));
-            $this->orderRepository->save($order);
-        } catch (\Exception $e) {}
     }
 }
--- Observer/CheckoutSubmitAllAfter.php
+++ Observer/CheckoutSubmitAllAfter.php
@@ -1,30 +1,36 @@
 <?php
 
-/**
- * This file is part of the Airwallex Payments module.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade
- * to newer versions in the future.
- *
- * @copyright Copyright (c) 2021 Magebit, Ltd. (https://magebit.com/)
- * @license   GNU General Public License ("GPL") v3.0
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Airwallex\Payments\Observer;
 
+use Airwallex\Payments\Model\Client\Request\PaymentIntents\Get;
 use Airwallex\Payments\Model\Traits\HelperTrait;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Sales\Model\Order;
+use Magento\Sales\Model\OrderRepository;
 
 class CheckoutSubmitAllAfter implements ObserverInterface
 {
     use HelperTrait;
+
+    /**
+     * @var OrderRepository
+     */
+    public OrderRepository $orderRepository;
+
+    /**
+     * @var Get
+     */
+    public Get $intentGet;
+
+    public function __construct(
+        OrderRepository $orderRepository,
+        Get $intentGet
+    )
+    {
+        $this->orderRepository = $orderRepository;
+        $this->intentGet = $intentGet;
+    }
     /**
      * @param Observer $observer
      *
@@ -33,12 +39,47 @@ class CheckoutSubmitAllAfter implements ObserverInterface
      */
     public function execute(Observer $observer): void
     {
+        /** @var \Magento\Sales\Model\Order $order */
         $order = $observer->getOrder();
+        /** @var string $intentId */
+        if ($intentId = $order->getPayment()->getAdditionalInformation('intent_id')) {
+            $this->addAVSResultToOrder($order, $intentId);
+        }
         if ($this->isRedirectMethodConstant($order->getPayment()->getMethod())) {
             $comment = sprintf('Status changed to %s, payment method is %s.', Order::STATE_PENDING_PAYMENT, 
                 $order->getPayment()->getMethod());
             $order->addCommentToStatusHistory(__($comment));
-            $order->setState(Order::STATE_PENDING_PAYMENT)->setStatus(Order::STATE_PENDING_PAYMENT)->save();
+            $order->setState(Order::STATE_PENDING_PAYMENT)->setStatus(Order::STATE_PENDING_PAYMENT);
         }
+        $this->orderRepository->save($order);
+    }
+
+
+    private function addAVSResultToOrder(Order $order, string $intentId)
+    {
+        $histories = $order->getStatusHistories();
+        if (!$histories) {
+            return;
+        }
+        $log = $src = '[Verification] ';
+        foreach ($histories as $history) {
+            $history->getComment();
+            if (strstr($history->getComment(), $log)) return;
+        }
+        try {
+            $resp = $this->intentGet->setPaymentIntentId($intentId)->send();
+            $respArr = json_decode($resp, true);
+            $brand = $respArr['latest_payment_attempt']['payment_method']['card']['brand'] ?? '';
+            if ($brand) $brand = ' Card Brand: ' . strtoupper($brand) . '.';
+            $last4 = $respArr['latest_payment_attempt']['payment_method']['card']['last4'] ?? '';
+            if ($last4) $last4 = ' Card Last Digits: ' . $last4 . '.';
+            $avs_check = $respArr['latest_payment_attempt']['authentication_data']['avs_result'] ?? '';
+            if ($avs_check) $avs_check = ' AVS Result: ' . $avs_check . '.';
+            $cvc_check = $respArr['latest_payment_attempt']['authentication_data']['cvc_result'] ?? '';
+            if ($cvc_check) $cvc_check = ' CVC Result: ' . $cvc_check . '.';
+            $log .= $brand . $last4 . $avs_check . $cvc_check;
+            if ($log === $src) return;
+            $order->addCommentToStatusHistory(__($log));
+        } catch (\Exception $e) {}
     }
 }
