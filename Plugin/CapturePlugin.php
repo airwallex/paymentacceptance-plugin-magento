999cfc0480e1cb54bcbba536efc083481b98d670
leon-zhang-awx <leon.zhang.ctr@airwallex.com>
2024-10-16 18:21:54 +0800

    force authorized amount is greater than 0 before capture

---
+++ Plugin/CapturePlugin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Airwallex\Payments\Plugin;
+
+use Airwallex\Payments\Model\PaymentIntentRepository;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Sales\Api\Data\OrderPaymentInterface;
+use Magento\Sales\Model\Order\Payment\Operations\CaptureOperation;
+use Magento\Sales\Api\Data\InvoiceInterface;
+
+class CapturePlugin
+{
+    /**
+     * @throws LocalizedException
+     */
+    public function beforeCapture(CaptureOperation $subject, OrderPaymentInterface $payment, InvoiceInterface $invoice)
+    {
+        $orderId = $payment->getOrder()->getId();
+        $repository = ObjectManager::getInstance()->get(PaymentIntentRepository::class);
+        $record = $repository->getByOrderId($orderId);
+        if (!$record) {
+            return;
+        }
+        if (empty($payment->getAmountAuthorized())) {
+            throw new LocalizedException(__('Authorized amount must be greater than 0.'));
+        }
+    }
+}
