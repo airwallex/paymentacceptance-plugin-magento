e74b87d4c8954a799b9544ba087f035d7c489917
leon-zhang-awx <leon.zhang.ctr@airwallex.com>
2024-11-14 10:57:41 +0800

    fix ui

--- view/frontend/web/js/view/payment/redirect-method.js
+++ view/frontend/web/js/view/payment/redirect-method.js
@@ -56,6 +56,11 @@ define([
                     window.awxBillingAddress = JSON.stringify(newAddress);
                 });
                 quote.paymentMethod.subscribe((newMethod) => {
+                    if (newMethod && newMethod.method.indexOf('airwallex_') === 0) {
+                        $(".totals.charge").hide();
+                    } else {
+                        $(".totals.charge").show();
+                    }
                     this.hideYouPay();
                     this.validationError('');
                     this.testPaymentMethod();
@@ -131,7 +136,6 @@ define([
                 }
                 $('.airwallex._active .checkout').removeClass('disabled');
                 if (sourceCurrency === targetCurrency) return true;
-                // if (targetCurrency === window.checkoutConfig.quoteData.base_currency_code && $(".totals.charge").length) return true;
                 let msg = "<span style='color: rgba(26, 29, 33, 1); font-weight: bold;'>We have converted the currency to " + targetCurrency + " so you can use Klarna.</span>";
                 this.validationError(msg);
 
@@ -167,36 +171,26 @@ define([
                 `);
             }
             $(youPayElement).html(`
-                <th class="mark" scope="row" style="padding-top: 26px;">
-                    <strong style="font-size: 1.8rem">` + $t('You Pay') + `</strong>
+                <th class="mark" scope="row" style="padding-top: 33px;">
+                    <span style="font-size: 1.8rem; font-weight: 600;">` + $t('You Pay') + `</strong>
                 </th>
                 <td class="amount">
                     <div class="switcher-tip">
                         <div style="color: rgba(108, 116, 127, 1); margin-right: 5px;">1 ${switchers.payment_currency} = ${switchers.client_rate} ${switchers.target_currency}</div>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
-                            <path fillRule="evenodd" clipRule="evenodd" d="M10.8751 6.00603C11.2227 5.84685 11.641 5.97553 11.836 6.31338C12.0431 6.6721 11.9202 7.13079 11.5615 7.33789L9.93769 8.27539C9.57897 8.4825 9.12028 8.3596 8.91317 8.00088L7.97567 6.37708C7.76857 6.01836 7.89147 5.55967 8.25019 5.35256C8.60891 5.14545 9.0676 5.26836 9.27471 5.62708L9.36849 5.78951C9.25886 4.02452 7.79267 2.62695 6.00007 2.62695C5.0122 2.62695 4.12347 3.05137 3.50626 3.72782L2.44482 2.66638C3.33417 1.71884 4.598 1.12695 6.00007 1.12695C8.69245 1.12695 10.8751 3.30957 10.8751 6.00195C10.8751 6.00331 10.8751 6.00467 10.8751 6.00603ZM1.12576 6.08873L1.12513 6.0891C0.766406 6.2962 0.307713 6.1733 0.100606 5.81458C-0.106501 5.45586 0.0164058 4.99717 0.375125 4.79006L1.99892 3.85256C2.35764 3.64545 2.81633 3.76836 3.02344 4.12708L3.96094 5.75088C4.16805 6.1096 4.04514 6.56829 3.68642 6.77539C3.3277 6.9825 2.86901 6.8596 2.6619 6.50088L2.66152 6.50022C2.90238 8.12792 4.30533 9.37695 6 9.37695C6.85293 9.37695 7.63196 9.06056 8.22613 8.53874L9.28834 9.60095C8.42141 10.3935 7.26716 10.877 6 10.877C3.3366 10.877 1.17206 8.74108 1.12576 6.08873Z" fill="#B0B6BF" />
+                        <svg width="12" height="24" viewBox="0 0 12 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <line x1="6.5" y1="2.18557e-08" x2="6.5" y2="6" stroke="#E8EAED"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M10.8751 12.006C11.2227 11.8469 11.641 11.9755 11.836 12.3134C12.0431 12.6721 11.9202 13.1308 11.5615 13.3379L9.93769 14.2754C9.57897 14.4825 9.12028 14.3596 8.91317 14.0009L7.97567 12.3771C7.76857 12.0184 7.89147 11.5597 8.25019 11.3526C8.60891 11.1455 9.0676 11.2684 9.27471 11.6271L9.36849 11.7895C9.25886 10.0245 7.79267 8.62695 6.00007 8.62695C5.0122 8.62695 4.12347 9.05137 3.50626 9.72782L2.44482 8.66638C3.33417 7.71884 4.598 7.12695 6.00007 7.12695C8.69245 7.12695 10.8751 9.30957 10.8751 12.002C10.8751 12.0033 10.8751 12.0047 10.8751 12.006ZM1.12576 12.0887L1.12513 12.0891C0.766406 12.2962 0.307713 12.1733 0.100606 11.8146C-0.106501 11.4559 0.0164058 10.9972 0.375125 10.7901L1.99892 9.85256C2.35764 9.64545 2.81633 9.76836 3.02344 10.1271L3.96094 11.7509C4.16805 12.1096 4.04514 12.5683 3.68642 12.7754C3.3277 12.9825 2.86901 12.8596 2.6619 12.5009L2.66152 12.5002C2.90238 14.1279 4.30533 15.377 6 15.377C6.85293 15.377 7.63196 15.0606 8.22613 14.5387L9.28834 15.6009C8.42141 16.3935 7.26716 16.877 6 16.877C3.3366 16.877 1.17206 14.7411 1.12576 12.0887Z" fill="#B0B6BF"/>
+                            <line x1="6.5" y1="18" x2="6.5" y2="24" stroke="#E8EAED"/>
                         </svg>
                     </div>
                     <div class="awx-amount">${switchers.target_currency} ${switchers.target_amount}</div>
                 </td>
             `);
             $(youPayElement).show();
-            $(".totals.charge").hide();
         },
 
         hideYouPay() {
-            // if (!$(".totals.charge").length) {
-            //     $(".table-totals tbody").append(`
-            //         <tr class="totals charge awx">
-            //             <th class="mark" data-bind="i18n: basicCurrencyMessage" scope="row">You will be charged for</th>
-            //             <td class="amount">
-            //                 <span class="price" data-bind="text: getBaseValue(), attr: {'data-th': basicCurrencyMessage}" data-th="You will be charged for">${parseFloat(window.checkoutConfig..base_grand_total).toFixed(2)} ${window.checkoutConfig.quoteData.base_currency_code}</span>
-            //             </td>
-            //         </tr>
-            //     `);
-            // }
             $('.awx-you-pay').hide();
-            $(".totals.charge").show();
         },
 
         showBillingAddress() {
